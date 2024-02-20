a38b05d82f5169a68f363272b408f44338c06117
hugo.p <jongyeonpark.dev@gmail.com>
2024-02-21 00:05:29 +0900

    notice.py updated

--- notice.py
+++ notice.py
@@ -1,11 +1,13 @@
 class Notice:
-  def __init__(self, title, num, href, author, noticeDate):
+  def __init__(self, title, num, href, author, noticeDate, type, source):
     self.title = title
     self.num = num
     self.href = href
     self.author = author
     self.noticeDate = noticeDate
+    self.type = type
+    self.source = source
 
   def __str__(self):
-    return 'title: {title}, num: {num}, href: {href}, author: {author}, noticeDate: {noticeDate}'.format(title=self.title, num=self.num, href=self.href, author=self.author, noticeDate=self.noticeDate)
+    return f"Notice({self.title}, {self.num}, {self.href}, {self.author}, {self.noticeDate}, {self.type}, {self.source})"
   
