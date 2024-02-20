0da53684e2583b9a25e99795abecdca2638cd66d
hugopark <jongyeonpark.dev@gmail.com>
2024-02-20 11:09:04 +0900

    db properties added

--- dbConnect.py
+++ dbConnect.py
@@ -1,6 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import yaml
 
+# Load the database configuration from the file
+with open('application-secret.yaml', 'r') as f:
+    dbConfig = yaml.safe_load(f)
+
+userName = dbConfig['db']['userName']
+password = dbConfig['db']['password']
+host = dbConfig['db']['host']
+port = dbConfig['db']['port']
+dbName = dbConfig['db']['dbName']
 
 
 url = "mysql+pymysql://'{userName}':'{password}'@{host}:{port}/{dbName}?charset=utf8".format(userName=userName, password=password, host=host, port=port, dbName=dbName)
